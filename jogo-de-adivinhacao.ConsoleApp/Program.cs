d279627ac7539ff16bc2091de3d790d8e306efdb
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-09 16:04:07 -0300

    Implementada a função de validar e exibir mensagem.

--- jogo-de-adivinhacao.ConsoleApp/Program.cs
+++ jogo-de-adivinhacao.ConsoleApp/Program.cs
@@ -3,20 +3,57 @@
 //2. Nosso jogo deve gerar um número secreto aleatório.
 //3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem.
 
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Security.Cryptography; //Quero usar a biblioteca padrão do sistema relacionada a criptografia.
 
-Console.WriteLine("===================");
-Console.WriteLine("Jogo de Adivinhação");
-Console.WriteLine("===================");
+bool deveContinuar = true;
 
-int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21); //Gerando números aleatórios com entre intervalos.
+while (deveContinuar)
+{
+    Console.Clear();
 
-Console.Write("Digite um número entre 1 e 20: ");
-string? chute = Console.ReadLine(); //Pode ou não vir vazia
+    Console.WriteLine("===================");
+    Console.WriteLine("Jogo de Adivinhação");
+    Console.WriteLine("===================");
 
-Console.WriteLine($"O valor digitado foi: {chute}");
-Console.WriteLine($"O valor aleatório era: {numeroAleatorio}");
+    int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21); //Gerando números aleatórios com entre intervalos.
+
+    Console.Write("Digite um número entre 1 e 20: ");
+    int? chute = Convert.ToInt32(Console.ReadLine()); //Pode ou não vir vazia
+
+    if (chute == numeroAleatorio)
+    {
+        Console.WriteLine("===================");
+        Console.WriteLine("Parabéns Você Acertou!");
+        Console.WriteLine("===================");
+    }
+
+    else if (chute > numeroAleatorio)
+    {
+        Console.WriteLine("===================");
+        Console.WriteLine("O Número Digitado Foi Maior que o Número Secreto! Tente chutar valores menores...");
+        Console.WriteLine("===================");
+    }
+    else
+    {
+        Console.WriteLine("===================");
+        Console.WriteLine("O Número Digitado Foi Menor que o Número Secreto! Tente chutar valores maiores...");
+        Console.WriteLine("===================");
+
+    }
+
+    Console.WriteLine("Desejea Continuar? (S/N): ");
+    string? opcaoContinuar = Console.ReadLine();
+
+    if (opcaoContinuar.ToUpper() != "S")
+    {
+        break;
+    }
+
+    Console.ReadLine();
 
 
-Console.ReadLine();
-
+}
