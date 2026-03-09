041aa6e6015bf2d8931e90840f15e57b26b4553a
Pedro Henrique dos Santos <pedrohenriquedswork@gmail.com>
2026-03-09 15:18:28 -0300

    Adicionado o processamento de input do usuário

--- jogo-de-adivinhacao.ConsoleApp/Program.cs
+++ jogo-de-adivinhacao.ConsoleApp/Program.cs
@@ -1,3 +1,16 @@
-﻿Console.WriteLine("===================");
+﻿//Objetivos / Passo a passo
+//1. Nosso jogo deve aceitar o input do jogador e exibir o valor digitado.
+//2. Nosso jogo deve gerar um número secreto aleatório.
+//3. Nosso jogo deve validar a tentativa do jogador e exibir uma mensagem.
+
+Console.WriteLine("===================");
 Console.WriteLine("Jogo de Adivinhação");
 Console.WriteLine("===================");
+
+Console.Write("Digite um número entre 1 e 20: ");
+string? chute = Console.ReadLine(); //Pode ou não vir vazia
+
+Console.WriteLine($"O valor digitado foi: {chute}");
+
+Console.ReadLine();
+
